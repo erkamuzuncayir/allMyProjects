5598c150b07f4ed4fd49e650ca6245d61acef956
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2022-06-08 00:10:30 +0300

    Updating player control mechanics.

--- Unity Projects/Project Swipe It/Assets/Scripts/PlayerController.cs
+++ Unity Projects/Project Swipe It/Assets/Scripts/PlayerController.cs
@@ -7,96 +7,88 @@ public enum SIDE { Left, Mid, Right }
 
 public class PlayerController : MonoBehaviour
 {
-    public SIDE m_Side = SIDE.Mid;
-    private float newXPos = 0f;
-    [HideInInspector]
-    public bool swipeLeft, swipeRight, swipeUp, swipeDown;
-    public float xValue;
     private CharacterController playerController;
     private Animator m_Animator;
-    private float x;
-    public float strafeSpeed;
-    public float jumpPower = 0.015f;
-    private float y;
-    [SerializeField]
-    private float gravity = 0.002f;
-    [SerializeField]
-    private float runSpeed = 5.0f;
-    private Vector3 moveDirection = Vector3.zero;
+    public SIDE m_Side = SIDE.Mid;
+    private bool swipeLeft, swipeRight, swipeUp;
+    private float xValue = 2;
+    private float strafeSpeed = 1;
+    [SerializeField] private float jumpSpeed;
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 goalPosition = Vector3.zero;
+
+
+
+
+    public float jumpForce = 20f;
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 1f;
+    float velocity;
+    Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<CharacterController>();
         m_Animator = GetComponent<Animator>();
+        startPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerController.isGrounded)
+        velocity = gravity * Time.deltaTime;
+        float lerpValue = strafeSpeed * Time.deltaTime;
+
+
+        // Input settings
+        swipeLeft = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
+        swipeRight = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+        swipeUp = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+
+        // Right and left movement
+        if (swipeLeft)
         {
-            swipeLeft = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
-            swipeRight = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
-            swipeUp = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
-            if (swipeLeft)
+            if (m_Side == SIDE.Mid)
             {
-                if (m_Side == SIDE.Mid)
-                {
-                    m_Animator.Play("strafeLeft");
-
-                    newXPos = -xValue;
-                    m_Side = SIDE.Left;
-                }
-                else if (m_Side == SIDE.Right)
-                {
-                    m_Animator.Play("strafeLeft");
-
-                    newXPos = 0;
-                    m_Side = SIDE.Mid;
-                }
+                m_Animator.Play("strafeLeft");
+                goalPosition.x = -xValue;
+                m_Side = SIDE.Left;
             }
-            if (swipeRight)
+            else if (m_Side == SIDE.Right)
             {
-                if (m_Side == SIDE.Mid)
-                {
-                    m_Animator.Play("strafeRight");
-
-                    newXPos = xValue;
-                    m_Side = SIDE.Right;
-                }
-                else if (m_Side == SIDE.Left)
-                {
-                    m_Animator.Play("strafeRight");
-
-                    newXPos = 0;
-                    m_Side = SIDE.Mid;
-                }
-            }
-            x = Mathf.Lerp(x, newXPos, Time.deltaTime * strafeSpeed);
-            moveDirection = new Vector3(x - transform.position.x, y * Time.deltaTime, 0);
-            if (swipeUp)
-            {
-                moveDirection.y = jumpPower;
-                m_Animator.Play("jump");
+                m_Animator.Play("strafeLeft");
+                goalPosition.x = 0;
+                m_Side = SIDE.Mid;
             }
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        playerController.Move(moveDirection);
+        if (swipeRight)
+        {
+            if (m_Side == SIDE.Mid)
+            {
+                m_Animator.Play("strafeRight");
+                goalPosition.x = xValue;
+                m_Side = SIDE.Right;
+            }
+            else if (m_Side == SIDE.Left)
+            {
+                m_Animator.Play("strafeRight");
+                goalPosition.x = 0;
+                m_Side = SIDE.Mid;
+            }
+        }
+        // Jump movement
 
-        //float horizontalInput = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        //this.transform.Translate(horizontalInput, 0, runSpeed * Time.deltaTime);
+            if (swipeUp)
+            {
+            goalPosition.y = 500;
+            }
+
+            transform.position = Vector3.Lerp(startPosition, goalPosition, lerpValue);
+            startPosition = gameObject.transform.position;
+            if(goalPosition.y > 0)
+        {
+            goalPosition.y -= goalPosition.y - 0.1f;
+        }
     }
-    //public void Jump()
-    //{
-    //    if (swipeUp)
-    //    {
-    //        y = jumpPower;
-    //        m_Animator.Play("jump");
-    //    }
-    //    else
-    //    {
-    //        y -= jumpPower * 2 * Time.deltaTime;
-    //    }
-    //}
 }
--- Unity Projects/Project Swipe It/Assets/Scripts/SpawnManager.cs
+++ Unity Projects/Project Swipe It/Assets/Scripts/SpawnManager.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs;
+    public GameObject[] RoadPieces = new GameObject[5];
+    const float RoadLength = 10f;
+    const float RoadSpeed = 10f;
     private float[] spawnPosX = new float[] { -2f, 0f, 2f };
-    private Vector3 spawnPos = new Vector3(0,2,0);
+    private Vector3 spawnPos = new Vector3(0, 2, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     // Start is called before the first frame update
@@ -18,6 +21,16 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        foreach (GameObject road in RoadPieces)
+        {
+            Vector3 newRoadPos = road.transform.position;
+            newRoadPos.z -= RoadSpeed * Time.deltaTime;
+            if (newRoadPos.z < -RoadLength / 2)
+            {
+                newRoadPos.z += RoadLength;
+            }
+            road.transform.position = newRoadPos;
+        }
 
     }
     void SpawnObstacle()
